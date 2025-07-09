bf17af32da9e2384dee1829ff3d80817923cfa48
hritikashyftlabs <hritika.ratnam@shyftlabs.io>
2025-07-09 15:56:23 +0530

    commit

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
+// require('dotenv').config();
+// const express = require('express');
+// const app = express();
 
-require('dotenv').config();
+
+// app.use(express.json());
+
+
+// const authRoutes = require('./routes/authRoutes');
+// app.use('/api/auth', authRoutes);
+
+// const inventoryRoutes = require('./routes/inventoryRoutes');
+// app.use('/api/inventory', inventoryRoutes);
+
+// const PORT = process.env.PORT || 6000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
 const express = require('express');
+require('dotenv').config();
+const authRoutes = require('./routes/authRoutes');
+const inventoryRoutes = require('./routes/inventoryRoutes');
+
 const app = express();
 
-// Middlewares
 app.use(express.json());
 
-// Routes
-const authRoutes = require('./routes/authRoutes');
 app.use('/api/auth', authRoutes);
-
-// Server
-const inventoryRoutes = require('./routes/inventoryRoutes');
 app.use('/api/inventory', inventoryRoutes);
 
 const PORT = process.env.PORT || 6000;
