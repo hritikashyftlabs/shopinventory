8c4e9890d2546d8681325ed5ae600b0549274ecd
hritikashyftlabs <hritika.ratnam@shyftlabs.io>
2025-07-09 15:58:39 +0530

    change it

--- server.js
+++ server.js
@@ -1,23 +1,3 @@
-// require('dotenv').config();
-// const express = require('express');
-// const app = express();
-
-
-// app.use(express.json());
-
-
-// const authRoutes = require('./routes/authRoutes');
-// app.use('/api/auth', authRoutes);
-
-// const inventoryRoutes = require('./routes/inventoryRoutes');
-// app.use('/api/inventory', inventoryRoutes);
-
-// const PORT = process.env.PORT || 6000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-
-
 const express = require('express');
 require('dotenv').config();
 const authRoutes = require('./routes/authRoutes');
